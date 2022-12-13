e9874b3cd4f5263171ed46aff04a27259d651f9b
Jonathan Grimes <jsg2021@gmail.com>
2022-12-13 16:22:57 -0600

    feat: ensure all found tickets are not resolved

--- lib/success.ts
+++ lib/success.ts
@@ -113,6 +113,8 @@ export async function success(config: PluginConfig, context: GenerateNotesContex
 
   const jira = makeClient(config, context);
 
+  await ensureTicketsAreOpen(jira, tickets);
+
   const project = await jira.projects.getProject({ projectIdOrKey: config.projectId });
   const { id: releaseVersionId } = await findOrCreateVersion(config, context, jira, project.id, newVersionName, newVersionDescription);
 
@@ -130,3 +132,41 @@ export async function success(config: PluginConfig, context: GenerateNotesContex
 
   await Promise.all(edits);
 }
+
+
+async function ensureTicketsAreOpen(client: JiraClient, tickets: string[]): Promise<void> {
+    const results = [];
+  
+    for (const ticket of tickets) {
+      try {
+        const {fields} = await client.issues.getIssue({ issueIdOrKey: ticket, fields: ['summary', 'status', 'resolution']});
+  
+        if (fields.status?.statusCategory?.name === 'Done' && fields.resolution) {
+          results.push({
+            message: `*** Ticket ${ticket} is closed with resolution ${fields.resolution.name}. Reopen it to unblock the release.`,
+          });
+        }
+      } catch (e: unknown) {
+        const error = e as Error & { errorMessages?: string[] };
+        const message = error.errorMessages?.join(', ') || error?.message as string || e;
+        results.push({
+          ignore: true,
+          message: `>>> Could not fetch ticket ${ticket}, it will be ignored/skipped. (${message})`,
+        });
+      }
+  
+      if (results.some(x => !x.ignore)) {
+        throw new Error(`
+  
+  PRE-FLIGHT CHECKS FAILED: Some tickets are closed:
+  
+  ${results.map(x => x.message).join('\n')}
+  
+  
+  
+  
+  `);
+      }
+    }
+  
+  }
